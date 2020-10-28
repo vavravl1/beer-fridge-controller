7aa8790dd4de27c8ab931cb566e04db971d9d92b
vlada <vladislav.vavra@gmail.com>
2020-10-28 17:02:43 +0100

    Store list of values

--- src/main/kotlin/cz/vlada/beer/fridge/repo/LastValuesRepository.kt
+++ src/main/kotlin/cz/vlada/beer/fridge/repo/LastValuesRepository.kt
@@ -5,10 +5,14 @@ import kotlinx.serialization.json.Json
 import java.io.File
 import java.time.Instant
 import kotlinx.serialization.*
+import java.time.Duration
+import java.util.*
+import kotlin.collections.HashMap
 
 object LastValuesRepository {
+    private val oldestBufferRecord = Duration.ofMinutes(5)
     private val persistedData = File("last-values.json")
-    private val values: MutableMap<String, StoredValue>
+    private val values: MutableMap<String, List<StoredValue>>
 
     init {
         values = if(persistedData.exists()) {
@@ -19,11 +23,22 @@ object LastValuesRepository {
     }
 
     fun add(topic: String, value: String) {
-        values[topic] = StoredValue(Instant.now(), value)
+        val storeValue = StoredValue(Instant.now(), value)
+        if(values[topic] != null && values[topic]!!.isNotEmpty()) {
+            val data = values[topic]!!
+            var newData = listOf(storeValue) + data
+            if(newData.last().stored.plus(oldestBufferRecord).isBefore(Instant.now())) {
+                newData = newData.subList(0, newData.size - 1)
+            }
+            values[topic] = newData
+        } else {
+            values[topic] = LinkedList(listOf(storeValue))
+        }
+
         storeDataToDisk()
     }
 
-    fun get(topic: String): StoredValue? = values[topic]
+    fun get(topic: String): StoredValue? = values[topic]?.firstOrNull()
 
     private fun storeDataToDisk() {
         persistedData.writeText(Json.encodeToString(values))
