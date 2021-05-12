9934de73db78e21445935d564e7954490a4195e2
Vladislav Vavra <vladislav.vavra@gmail.com>
2021-05-12 08:28:36 +0200

    simplify beer freezer

--- src/main/kotlin/cz/vlada/beer/fridge/listener/FreezerController.kt
+++ src/main/kotlin/cz/vlada/beer/fridge/listener/FreezerController.kt
@@ -1,19 +1,14 @@
 package cz.vlada.beer.fridge.listener
 
-import cz.vlada.beer.fridge.LinearPrediction
 import cz.vlada.beer.fridge.repo.LastValuesRepository
 import org.eclipse.paho.client.mqttv3.MqttMessage
 import org.slf4j.LoggerFactory
-import java.time.Duration
-import java.time.Instant
 
 object FreezerController : MqttListener {
     private const val setLowTemperatureTopic = "node/BeerFreezer/thermometer/low/temperature/set"
     private const val setHighTemperatureTopic = "node/BeerFreezer/thermometer/high/temperature/set"
     private const val currentTemperatureTopic = "node/BeerFreezer/thermometer/0:1/temperature"
     private const val powerSwitchTopic = "shellies/beer_fridge_shelly/relay/1/command"
-
-    private val predictionWindow = Duration.ofMinutes(10)
 
     private val log = LoggerFactory.getLogger("cz.vlada.beer.fridge.listener.FridgeMqttListener")
 
@@ -36,32 +31,24 @@ object FreezerController : MqttListener {
                 highTemperature = msg
             }
             currentTemperatureTopic -> {
-                val temperatureHistory = LastValuesRepository.getAll(topic)
-                if(temperatureHistory.isNotEmpty()) {
-                    val predictedValue = LinearPrediction.getValueAtTime(
-                        temperatureHistory,
-                        Instant.now().plus(predictionWindow)
+                log.debug(
+                    "BeerFreezer - temperature: $msg, " +
+                            "lowTemperature = $lowTemperature, " +
+                            "highTemperature = $highTemperature"
+                )
+                if (msg < lowTemperature) {
+                    log.info(
+                        "Turning BeerFreezer off - temperature: $msg, " +
+                                "lowTemperature: $lowTemperature "
                     )
-                    log.debug(
-                        "BeerFreezer - temperature: $msg, " +
-                                "predicted = $predictedValue " +
-                                "lowTemperature = $lowTemperature, " +
-                                "highTemperature = $highTemperature"
+                    publish(powerSwitchTopic, "off")
+                }
+                if (msg > highTemperature) {
+                    log.info(
+                        "Turning BeerFreezer on - temperature: $msg, " +
+                                "highTemperature: $highTemperature, "
                     )
-                    if (predictedValue < lowTemperature) {
-                        log.info(
-                            "Turning BeerFreezer off - temperature: $msg, " +
-                                    "lowTemperature: $lowTemperature, " +
-                                    "predicted = $predictedValue")
-                        publish(powerSwitchTopic, "off")
-                    }
-                    if (predictedValue > highTemperature) {
-                        log.info(
-                            "Turning BeerFreezer on - temperature: $msg, " +
-                                    "highTemperature: $highTemperature, " +
-                                    "predicted = $predictedValue")
-                        publish(powerSwitchTopic, "on")
-                    }
+                    publish(powerSwitchTopic, "on")
                 }
             }
         }
